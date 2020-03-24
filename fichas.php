03192271570646ff732c78ef406e2ce044fe4263
Magi2497 <55759338+Magi2497@users.noreply.github.com>
2020-03-24 19:55:16 +0100

    Add files via upload

--- fichas.php
+++ fichas.php
@@ -1,24 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Ficha animes</title>
 
+<?php include "includes/navbar.php"?>
+</head>
+<body>
+<?php
+/* Activamos los flags para que nos muestre los errores.
+Este código no debería ir en producción */
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+?>
 
+<?php
+ if (isset($_GET['id'])){
+  $id = $_GET['id'];
+  $db = new Sqlite3('animes.db');
+  $stm = $db->prepare("SELECT * FROM animes where id=?");
+  $stm->bindParam(1, $id);
+ 
+  $res = $stm->execute();
 
+  while ($row = $res->fetchArray()) {
+     $nom = $row['nombre'];
+     $uuid = $row['uuid']; 
+     $genero = $row['genero'];
+     $descripcion = $row['descrpipcion'];
+     $img = $row['img'];
+  }
+}
+  else {
+    $nom = 'not found';
+    $uuid = 'not found'; 
+    $genero = 'not found';
+    $descripcion = 'not found';
+    $img = 'not found';
 
-
+  }
+?>
 <div class="col-lg-4 mb-4">
             <div class="entry2">
-              <a href="single.html"><img src="<?php echo $cotxe['url'] ?>"></a>
+              <a href="single.html"><img src="<?php echo $img?>"></a>
               <div class="excerpt">
                 
-              <h2><a href="single.html"><?php echo $animes['titulo'] ?></a></h2>
-              <span class="post-category text-white bg-secondary mb-3"> <?php echo $animes['classe'] ?></span>
+              <h2><a href="single.html"><?php echo $nom?></a></h2>
+              <span class="post-category text-white bg-secondary mb-3"> <?php echo $genero ?></span>
 
               
               <div class="post-meta align-items-center text-left clearfix">
-                <span class="d-inline-block mt-1"><?php echo $animes['autor'] ?></a></span>
+                <span class="d-inline-block mt-1"></a></span>
               </div>
               
-                <p><?php echo $animes['descripcion'] ?> </p>
+                <p><?php echo $descripcion ?> </p>
                 
               </div>
             </div>
           </div>
 
+</div>
+</div>
+
+
+<?php include "includes/footer.php"?>
+</body>
+</html>
---
+++ includes/footer.php
@@ -0,0 +1,55 @@
+<div class="site-footer">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md-4">
+            <h3 class="footer-heading mb-4">Sobre nosotros</h3>
+            <p>Somos un grupo de compañeros de clase con la intencion de crear una web sobre animes</p>
+          </div>
+          <div class="col-md-3 ml-auto">
+            <ul class="list-unstyled float-left mr-5">
+              <li><a href="#">Quienes somos</a></li>
+              <li><a href="#">Contacto</a></li>
+              <li><a href="#">Apoyanos</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4">
+            
+
+            <div>
+              <h3 class="footer-heading mb-4">Buscanos</h3>
+              <p>
+                <a href="#"><span class="icon-twitter p-2"></span></a>
+                <a href="#"><span class="icon-instagram p-2"></span></a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-12 text-center">
+            <p>
+              Copyright &copy; <script>document.write(new Date().getFullYear());</script> Todos los derechos reservados
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+
+  
+
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/aos.js"></script>
+
+  <script src="js/main.js"></script>
+
+
+  
