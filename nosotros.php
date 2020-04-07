3d11b3ed28180eb019d2f662d9eaf7644353da46
Magi2497 <55759338+Magi2497@users.noreply.github.com>
2020-04-07 18:00:14 +0200

    Add files via upload

--- nosotros.php
+++ nosotros.php
@@ -20,58 +20,9 @@
     <link rel="stylesheet" href="css/style.css">
   </head>
   <body>
-  
-  <div class="site-wrap">
-
-    <div class="site-mobile-menu">
-      <div class="site-mobile-menu-header">
-        <div class="site-mobile-menu-close mt-3">
-          <span class="icon-close2 js-menu-toggle"></span>
-        </div>
-      </div>
-      <div class="site-mobile-menu-body"></div>
-    </div>
-    
-    <header class="site-navbar" role="banner">
-      <div class="container-fluid">
-        <div class="row align-items-center">
-          
-          <div class="col-12 search-form-wrap js-search-form">
-            <form method="get" action="#">
-              <input type="text" id="s" class="form-control" placeholder="Search...">
-              <button class="search-btn" type="submit"><span class="icon-search"></span></button>
-            </form>
-          </div>
-
-          <div class="col-4 site-logo">
-            <a href="index.html" class="text-black h2 mb-0">ADJON</a>
-          </div>
-
-          <div class="col-8 text-right">
-            <nav class="site-navigation" role="navigation">
-              <ul class="site-menu js-clone-nav mr-auto d-none d-lg-block mb-0">
-                <li>   <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  GENERO
-                </a><div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                  <a class="dropdown-item" href="#">Isekai</a>
-                  <a class="dropdown-item" href="#">Drama</a>
-                  <a class="dropdown-item" href="#">Seinen</a>
-                  <a class="dropdown-item" href="#">Shonen</a>
-                  <a class="dropdown-item" href="#">Spokon</a>
-                </div></li>
-                <li><a href="category.html">XXX</a></li>
-                <li><a href="category.html">XXX</a></li>
-                <li><a href="category.html">XXX</a></li>
-                <li><a href="category.html">XXX</a></li>
-                <li><a href="category.html">XXX</a></li>
-                <li class="d-none d-lg-inline-block"><a href="#" class="js-search-toggle"><span class="icon-search"></span></a></li>
-              </ul>
-            </nav>
-            <a href="#" class="site-menu-toggle js-menu-toggle text-black d-inline-block d-lg-none"><span class="icon-menu h3"></span></a></div>
-          </div>
-
-      </div>
-    </header>
+  <?php include "includes/navbar.php"?>
+ 
+   
     
     
     <div class="site-cover site-cover-sm same-height overlay single-page" style="background-image: url('images/quienes_somos.jpg');">
@@ -79,7 +30,7 @@
         <div class="row same-height justify-content-center">
           <div class="col-md-12 col-lg-10">
             <div class="post-entry text-center">
-              <h1 class="">Quienes somos</h1>
+              <h1 class="">Quienes somos?</h1>
             </div>
           </div>
         </div>
@@ -91,7 +42,7 @@
       <div class="container">
         <div class="row">
           <div class="col-md-6 order-md-2">
-            <img src="images/img_1.jpg" alt="Image" class="img-fluid">
+            <img src="images/anime.jpg" alt="Image" class="img-fluid">
           </div>
           <div class="col-md-5 mr-auto order-md-1">
             <h2>Porque estamos aqui</h2>
@@ -116,9 +67,9 @@
         </div>
         <div class="row">
           <div class="col-md-6 col-lg-4 mb-5 text-center">
-            <img src="images/person_1.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-4">
+            <img src="images/mateu.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-4">
             <h2 class="mb-3 h5">Mateu Salva Riutort</h2>
-            <p>Nuestro pelocho</p>
+            <p>Servicio técnico</p>
 
             <p class="mt-5">
               <a href="#" class="p-3"><span class="icon-instagram"></span></a>
@@ -127,9 +78,9 @@
           </div>
 
           <div class="col-md-6 col-lg-4 mb-5 text-center">
-            <img src="images/person_2.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-4">
+            <img src="images/facundo.png" alt="Image" class="img-fluid w-50 rounded-circle mb-4">
             <h2 class="mb-3 h5">Facundo San Martin Curbelo</h2>
-            <p>El que tiene mala hostia en los animes</p>
+            <p>Programador</p>
 
             <p class="mt-5">
               <a href="#" class="p-3"><span class="icon-instagram"></span></a>
@@ -138,9 +89,9 @@
           </div>
 
           <div class="col-md-6 col-lg-4 mb-5 text-center">
-            <img src="images/person_3.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-4">
-            <h2 class="mb-3 h5">Magi Payeras Andreu</h2>
-            <p>El que lo sabe todo en los animes</p>
+            <img src="images/magi.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-4">
+            <h2 class="mb-3 h5">Magí Payeras Andreu</h2>
+            <p>Relaciones públicas</p>
 
             <p class="mt-5">
               <a href="#" class="p-3"><span class="icon-instagram"></span></a>
@@ -149,9 +100,9 @@
           </div>
 
           <div class="col-md-6 col-lg-4 mb-5 text-center">
-            <img src="images/person_3.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-4">
+            <img src="images/luis.jpg" alt="Image" class="img-fluid w-50 rounded-circle mb-4">
             <h2 class="mb-3 h5">Luis Francisco Alba Vargas</h2>
-            <p>El que cuenta la historia</p>
+            <p>Mantenimiento</p>
 
             <p class="mt-5">
               <a href="#" class="p-3"><span class="icon-instagram"></span></a>
@@ -162,96 +113,7 @@
       </div>
     </div>
     
-    <div class="site-section bg-light">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-6">
-            <img src="images/img_1.jpg" alt="Image" class="img-fluid">
-          </div>
-          <div class="col-md-5 ml-auto">
-            <h2>Aprende sobre nosotros</h2>
-            
-            <ul class="ul-check list-unstyled success">
-              <li>Somos igual de frikis que todos vosotros</li>
-              <li>Creamos la mejor pagina de animes</li>
-              <li>Vuestras peticiones seran escuchadas</li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-
-
-    <div class="site-section bg-white">
-      <div class="container">
-        <div class="row justify-content-center text-center">
-          <div class="col-md-5">
-            <div class="subscribe-1 ">
-              <h2>Suscribete para recibir noticias</h2>
-              <form action="#" class="d-flex">
-                <input type="text" class="form-control" placeholder="Enter your email address">
-                <input type="submit" class="btn btn-primary" value="Subscribe">
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    
-    <div class="site-footer">
-      <div class="container">
-        <div class="row mb-5">
-          <div class="col-md-4">
-            <h3 class="footer-heading mb-4">Sobre nosotros</h3>
-            <p>Somos un grupo de compañeros de clase con la intencion de crear una web sobre animes</p>
-          </div>
-          <div class="col-md-3 ml-auto">
-            <ul class="list-unstyled float-left">
-              <li><a href="#">Quienes somos</a></li>
-              <li><a href="#">Contacto</a></li>
-              <li><a href="#">Apoyanos</a></li>
-            </ul>
-          </div>
-          <div class="col-md-4">
-            
-
-            <div>
-              <h3 class="footer-heading mb-4">Buscanos</h3>
-              <p>
-                <a href="#"><span class="icon-twitter p-2"></span></a>
-                <a href="#"><span class="icon-instagram p-2"></span></a>
-              </p>
-            </div>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-12 text-center">
-            <p>
-              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-              Copyright &copy; <script>document.write(new Date().getFullYear());</script>Todos los derechos reservados
-              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-              </p>
-          </div>
-        </div>
-      </div>
-    </div>
-    
-  </div>
-
-  <script src="js/jquery-3.3.1.min.js"></script>
-  <script src="js/jquery-migrate-3.0.1.min.js"></script>
-  <script src="js/jquery-ui.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/owl.carousel.min.js"></script>
-  <script src="js/jquery.stellar.min.js"></script>
-  <script src="js/jquery.countdown.min.js"></script>
-  <script src="js/jquery.magnific-popup.min.js"></script>
-  <script src="js/bootstrap-datepicker.min.js"></script>
-  <script src="js/aos.js"></script>
-
-  <script src="js/main.js"></script>
-
+    <?php include "includes/footer.php"?>
  
   </body>
 </html>
